4d797b668ac7242f134fa0ac4b4adb6c4f2176f4
cztee <cztee96@gmail.com>
2024-04-03 19:07:14 +0000

    fixed the code

--- trigger.c
+++ trigger.c
@@ -21,11 +21,11 @@
 #include "fpga_osc.h"
 #include <time.h>
 
-#define ENOUGH ((CHAR_BIT * sizeof(int) - 1) / 3 + 2)
+#define BILLION 1000000000L
 
 //Buffer depth 
 const int BUF = 16*1024;
-const int N = 100; 			// desired length of trace (1,..., 16383)
+const int N = 80; 			// desired length of trace (1,..., 16383)
 const int decimation = 1; 	// decimation: [1;8;64;1024;8192;65536]
 
 int main(int argc, char *argv[]) 
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 	int trace_counts = atoi(argv[1]); 		// counts how many traces were sampled in for loop
 	int trig_ptr; 			// trigger pointer shows memory adress where trigger was met
 	int trig_test;			// trigger test checks if writing the trace has completed yet
-	int trigger_voltage= atoi(argv[2]); // enter trigger voltage in [V] as parameter [1V...~600 RP units]
+	float trigger_voltage= atof(argv[2]); // enter trigger voltage in [V] as parameter [1V...~600 RP units]
 	g_osc_fpga_reg_mem->cha_thr = osc_fpga_cnv_v_to_cnt(trigger_voltage); //sets trigger voltage
 	//time check
 	time_t endwait;    
@@ -81,13 +81,21 @@ int main(int argc, char *argv[])
         time_t seconds = (long int)atoi(argv[3]);
 
 	endwait = start_time + seconds;
-	printf("Endwait:%ld, Start Time: %ld\n",endwait, start_time);
-	printf("No of events:%d, Trig level:%d, Duration:%ld \n", trace_counts,trigger_voltage,seconds);
+	//printf("Endwait:%ld, Start Time: %ld\n",endwait, start_time);
+	//printf("No of events:%d, Trig level:%f, Duration:%ld \n", trace_counts,trigger_voltage,seconds);
 	/***************************/
 	/** MAIN ACQUISITION LOOP **/
 	/***************************/
+	
 	for (int trace_count=0; trace_count<trace_counts; trace_count++)
-	{	printf("%d\n",trace_count);
+	{	
+
+		// calculate how many microsecond elapsed per event 
+		uint64_t diff;
+		struct timespec start, end;
+
+		/* measure monotonic time */
+		clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
 		/*Set trigger, begin acquisition when condition is met*/
 		osc_fpga_arm_trigger(); //start acquiring, incrementing write pointer
 		osc_fpga_set_trigger(0x3); // where do you want your triggering from?
@@ -102,22 +110,22 @@ int main(int argc, char *argv[])
 	 	// Trigger always changes to 0 after acquisition is completed, write pointer stops incrementing
 	 	//->fpga.osc.h l66
 
-	printf("Checkpoint A\n");
+	//printf("Checkpoint A\n");
 	    /*Wait for the acquisition to finish = trigger is set to 0*/
      	trig_test=(g_osc_fpga_reg_mem->trig_source); // it gets the above trigger value
-	printf("Checkpoint B\n");
-	printf("%d\n",trig_test);
+	//printf("Checkpoint B\n");
+	//printf("%d\n",trig_test);
      	// if acquistion is not yet completed it should return the number you set above and 0 otherwise
      	while (trig_test!=0){ // with this loop the program waits until the acquistion is completed before cont.
      		trig_test=(g_osc_fpga_reg_mem->trig_source);
      	}
      	//->fpga_osc.c l366
 
-	printf("Checkpoint C\n");
+	//printf("Checkpoint C\n");
 	    trig_ptr = g_osc_fpga_reg_mem->wr_ptr_trigger; // get pointer to mem. adress where trigger was met
 	    //->fpga_osc.c l283
 	    osc_fpga_get_sig_ptr(&cha_signal, &chb_signal); 
-	    printf("Checkpoint 1\n");
+	    //printf("Checkpoint 1\n");
  	    //->fpga_osc.c l378
             //now read N samples from the trigger pointer location //
 	    int i;
@@ -135,13 +143,16 @@ int main(int argc, char *argv[])
         		fprintf(fp, "%d ", cha_signal[ptr]);
         	}
             }
-	   // printf("\n");
-	    fprintf(fp, "\t");
-	    char str[ENOUGH];
-	    sprintf(str, "%ld", time(NULL)-start_time);
-	    
-	    fprintf(fp,"%s",strcat( strcat("(",strcat(str,")")),"\n") );
-	   printf("Checkpoint 2\n"); 
+	    clock_gettime(CLOCK_MONOTONIC, &end);
+	    fprintf(fp, " ");
+	    //char str[ENOUGH];
+	    //sprintf(str, "%ld", time(NULL)-start_time);
+	    //printf("Almost!\n");
+	    fprintf(fp,"%d ", -9999); // this separates the data from timestamp
+	    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	    fprintf(fp,"%llu ",(long long unsigned int) diff);
+	    fprintf(fp, "\n");
+	   //printf("Checkpoint 2\n"); 
 	   if(endwait <time(NULL)){break;}
 	}
 
